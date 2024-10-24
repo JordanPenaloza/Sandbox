9baf540231acd4659b08261d992926d04dea650d
JordanPenaloza <jordan332033@gmail.com>
2024-10-23 22:36:58 -0700

    created decimal to binary and binary to decimal function

--- random pi shiz/base10tobinary.py
+++ random pi shiz/base10tobinary.py
@@ -1,9 +1,25 @@
-x = 123456789
-binary = ""
-while x != 0:
-    if x % 2 == 0:
-        binary = "0" + binary
-    else:
-        binary = "1" + binary
-    x = x // 2
-print(binary)
+
+def to_binary():
+    x = int(input("enter the decimal number: "))
+    binary = ""
+    while x != 0:
+        if x % 2 == 0:
+            binary = "0" + binary
+        else:
+            binary = "1" + binary
+        x = x // 2
+    print(binary)
+
+def to_decimal():
+    x = input("enter the binary number: ")
+    x = str(x)
+    decimal = 0
+    for i in range(len(x)):
+        decimal += int(x[i]) * 2 ** (len(x) - 1 - i)
+    print(decimal)
+
+function = input("Enter 'b' for binary to decimal or 'd' for decimal to binary: ")
+if function == "b":
+    to_decimal()
+elif function == "d":
+    to_binary()
